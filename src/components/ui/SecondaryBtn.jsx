4a78c1f1831017ae96d8ecc12db5d829a356d3c9
deceser <aufhaummtobsc@gmail.com>
2024-05-14 14:00:11 +0300

    3-3-

--- src/components/ui/SecondaryBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const button = ({title}) => {
-  return (
-    <a href="#" className='text-center bg-white border-[3px] shadow-lg rounded-md px-4 md:px-8 py-2 border-white smooth hover:border-black'>{title}</a>
-  )
-}
-
-export default button
--- src/layout/heroText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const heroText = ({children}) => {
-  return (
-    <div className='lg:p-28 md:p-12 p-6 leading-7'>{children}</div>
-  )
-}
-
-export default heroText
